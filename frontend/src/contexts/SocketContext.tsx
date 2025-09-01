eae93a812c04b458a3543d6f4cff1b4421026dc0
Brain <gsaboia@yahoo.com>
2025-09-01 15:10:18 -0300

    fix: type import

--- frontend/src/contexts/SocketContext.tsx
+++ frontend/src/contexts/SocketContext.tsx
@@ -1,6 +1,6 @@
-import React, { type ReactNode } from 'react';
+import React, { type ReactNode, useContext } from 'react';
 import { useSocket } from '../hooks/useSocket';
-import { SocketContext, SocketContextType } from './SocketContextType';
+import { SocketContext, type SocketContextType } from './SocketContextType';
 
 interface SocketProviderProps {
   children: ReactNode;
@@ -14,5 +14,13 @@ export const SocketProvider: React.FC<SocketProviderProps> = ({ children }) => {
       {children}
     </SocketContext.Provider>
   );
+};
+
+export const useSocketContext = (): SocketContextType => {
+  const context = useContext(SocketContext);
+  if (!context) {
+    throw new Error('useSocketContext must be used within a SocketProvider');
+  }
+  return context;
 };
 
